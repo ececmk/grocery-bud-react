ab3997d70837e3330b22ae0fdd4ca60d0407e19f
Ece Çakmak <86318154+ececmk@users.noreply.github.com>
2023-01-31 16:34:17 +0100

    alert done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,55 @@
 import React, { useState, useEffect } from 'react'
 import List from './List'
 import Alert from './Alert'
+import { FaSortNumericUpAlt } from 'react-icons/fa'
 
 function App() {
-  return <h2>grocery bud setup</h2>
+  const [name, setName] = useState('')
+  const [list, setList] = useState([])
+  const [isEditing, setIsEditing] = useState(false)
+  const [editID, setEditID] = useState(null)
+  const [alert, setAlert] = useState({
+    show:false, 
+    message:'',
+    type:'' })
+
+ const handleSubmit = (e) => {
+  e.preventDefault()
+  if(!name){
+   showAlert(true, 'danger', 'Please enter value.')
+  }else if (name && isEditing){
+    //deal with edit
+  }else {
+    //show alert
+    const newItem = { id: new Date().getTime().toString(), title: name };
+    setList([...list, newItem]);
+    setName('');
+  }
+ }
+
+ const showAlert = (show=false, type='', message='') => {
+  setAlert({show, type, message})
+ }
+
+ return (
+  <section className='section-center'>
+    <form className='grocery-form' onSubmit={handleSubmit}>
+      {alert.show && <Alert {...alert} removeAlert={showAlert} list={list} />}
+      <h3>Grocery bud</h3>
+      <div className='form-control'>
+        <input type='text' className='grocery' placeholder='e.g. eggs' value={name} onChange={(e) => setName(e.target.value)}/>
+        <button type='submit' className='submit-btn' >Submit</button>
+      </div>
+    </form>
+    {list.length > 0 && (
+      <div className='grocery-container'>
+      <List items={list} />
+      <button className='clear-btn'>Clear Items</button>
+    </div>
+    )}
+    
+  </section>
+ )
 }
 
 export default App;
